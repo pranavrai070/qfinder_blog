ebc6ea3d5b941ae4a708e06f221b2bc473222a46
pranavrai070 <pranavrai070@gmail.com>
2023-12-21 11:25:01 +0530

    ads.txt and meta tag added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,6 +6,7 @@ const Layout = ({ children }) => (
   <>
     <Head>
       <link rel="icon" href="/favicon.png" />
+      <meta name="google-adsense-account" content="ca-pub-2590982090650195"></meta>
       <title>Qfinder's Blog</title>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2590982090650195"
      crossorigin="anonymous"></script>
