fdf9f32038f390f63541bb5de1eb3fc041e4047a
pranavrai070 <pranavrai070@gmail.com>
2024-02-15 11:55:58 +0530

    google adsense updated

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,8 +8,6 @@ const Layout = ({ children }) => (
       <link rel="icon" href="/favicon.png" />
       <meta name="google-adsense-account" content="ca-pub-2590982090650195"></meta>
       <title>Qfinder's Blog</title>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2590982090650195"
-     crossorigin="anonymous"></script>
     </Head>
     <Header />
     {children}
